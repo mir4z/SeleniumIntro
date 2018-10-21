f93212e2be3dd5d420b3a42b71aaeb4eb1407fac
Konrad Wojtas <kwojtas.pl@gmail.com>
2018-10-22 00:29:19 +0200

    Added configuration properties - added missing BeforeClass

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,23 +1,36 @@
 package tests;
+
+import configuration.ConfigurationProperties;
+import configuration.PropertiesLoader;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import util.driver.DriverManager;
 import util.driver.DriverUtils;
 import util.navigation.ApplicationURLs;
 
+import java.util.Properties;
+
 import static util.navigation.ApplicationURLs.*;
 
 public class BaseTest {
+    @BeforeClass
+    public void beforeClass() {
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        Properties propertiesFromFile = propertiesLoader.getPropertiesFromFile("configuration.properties");
+        ConfigurationProperties.setProperties(propertiesFromFile);
 
+    }
     @BeforeMethod
     public void beforeTest() {
         DriverManager.getWebDriver();
         DriverUtils.setInitialConfiguration();
         DriverUtils.navigateToPage(MAIN_PAGE);
     }
+
     @AfterMethod
     public void afterTest() {
-       DriverManager.disposeDriver();
+        DriverManager.disposeDriver();
     }
 
 }
--- src/test/java/tests/SuccessLoginTest.java
+++ src/test/java/tests/SuccessLoginTest.java
@@ -2,12 +2,13 @@ package tests;
 
 import org.testng.annotations.Test;
 import page.objects.LoggedMainPage;
+import util.helperClasses.Login;
 
 public class SuccessLoginTest extends BaseTest {
 
     @Test
     public void check() {
-        LoggedMainPage loggedMainPage = new LoggedMainPage();
+        LoggedMainPage loggedMainPage = Login.logIn();
 
 
 
